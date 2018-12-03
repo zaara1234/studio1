42eb0fbc256f5bf1db408a581e1fa5810d50bd2a
Extrenious <Extreniu@gmail.com>
2018-12-03 17:39:40 -0500

    Update 12/3/18

--- index.js
+++ index.js
@@ -3,10 +3,16 @@
 var database = firebase.database();
 var databaseRef = database.ref("/");
 
-// Read the data from the database and take a snapshot of that data.
+
+$("#makeQues").click(function(){
+databaseRef.push({
+        "Topic": "Snowy or sunny", 
+        "Answers": ["Sunny","blizzard"] 
+  });
+  // Read the data from the database and take a snapshot of that data.
 databaseRef.once("value").then(function(snapshot) {
  // Use .val() to get the data from the snapshot.
  const directory = snapshot.val();
 console.log(directory);
 });
-
+});
