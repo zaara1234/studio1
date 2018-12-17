6dfa66eaaedd07a71689209e9e42658a24ad18bd
Zaara Afida <zaaraalam76.za@gamil.com>
2018-12-17 17:36:43 -0500

    fix comments

--- index.js
+++ index.js
@@ -3,6 +3,28 @@
 var database = firebase.database();
 var databaseRef = database.ref("/");
 
+function updateHtml(){
+  databaseRef.once("value").then(function(snapshot) {
+    var string = ""
+    snapshot.forEach(function(comments) {
+      var stringcomments = ""
+      var array = comments.val()["Answers"] 
+      if (array) {
+        array.forEach(function(e){
+          stringcomments += "<p>"+ e + "</p>"
+        }) 
+      }
+      
+      string += "<div id = \"" + comments.key + "\"><p>"+(comments.val()["Topic"]) +
+        "</p>" + stringcomments +" <input id = \"" + comments.key + 
+        "input\" class = \"commentinput\"> <button type= 'button' class='btn btn-secondary' \"makeComment\" onclick=\"updateComment('" + comments.key + "')\">Make a comment</button></div>"; 
+    });
+    $("#Questions").html(string);
+  });
+}
+
+updateHtml();
+
 $("#askquestions").click(function(){
 var questions= $("#questioninput").val();
   console.log(questions)
@@ -10,55 +32,30 @@ var questions= $("#questioninput").val();
           "Topic": questions, 
           "Answers": [] 
     });
-    databaseRef.once("value").then(function(snapshot) {
-      var string = ""
-      snapshot.forEach(function(comments) {
-         string += "<div id = \"" + comments.key + "\"><p>"+(comments.val()["Topic"])+ "</p> </div>";
-          
-      });
-      $("#Questions").html(string);
-    });
+    updateHtml();
+    $("#questioninput").val("");
   });
 
-databaseRef.once("value").then(function(snapshot) {
-  var string = ""
-  snapshot.forEach(function(comments) {
-    var stringcomments = ""
-    var array = comments.val()["Answers"] 
-    if (array) {
-      array.forEach(function(e){
-        stringcomments += "<p>"+ e + "</p>"
-      }) 
-    }
-    
-     string += "<div id = \"" + comments.key + "\"><p>"+(comments.val()["Topic"]) +
-      "</p>" + stringcomments +" <input id = \"" + comments.key + 
-      "input\" class = \"commentinput\"> <button type= 'button' class='btn btn-secondary' \"makeComment\" onclick=\"updateComment(\"" + comments.key + "\")\">Make a comment</button></div>"; 
-  });
-  $("#Questions").html(string);
-});
-
-
+var commentSnapshot;
 function updateComment(id){
   var ref = database.ref("/"+id)
   ref.once("value").then(function(snapshot) {
     // Use .val() to get the data from the snapshot.
-    const snapshot = snapshot.val();
-    console.log(snapshot)
+    commentSnapshot = snapshot.val();
+    var comment= $("#" + id + "input").val();
+    if (commentSnapshot.Answers) {
+      var commentArray = commentSnapshot.Answers
+      commentArray.push(comment)
+    }
+    else {
+      var commentArray = [comment]
+    }
+    ref.set({
+            "Topic": commentSnapshot.Topic, 
+            "Answers": commentArray
+    });
+    updateHtml();
    });
-  };
-$("button.makeComment").click(function(){
-  console.log("Clicked")
-  var comments= $("commentsinput").val();
-     databaseRef.push({
-            "Topic": comments, 
-            "Answers": [] 
-    });
-    //   // Read the data from the database and take a snapshot of that data.
-    // databaseRef.once("value").then(function(snapshot) {
-    //  // Use .val() to get the data from the snapshot.
-    //  const directory = snapshot.val();
-    // console.log(directory);
-    // });
-    });
+};
+
   
